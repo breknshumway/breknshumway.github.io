fb7b3bcbd26b89f4084a9a5e7306fd797b0a676a
breknshumway <74122095+breknshumway@users.noreply.github.com>
2020-11-08 15:15:18 -0700

    First Website Upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+
+function hsShow() {
+    var text = document.getElementById("hsPara");
+
+    if (text.style.display === "none") {
+        text.style.display = "block";
+    }
+    else {
+        text.style.display = "none";
+    }
+}
+
+
+function dtShow() {
+    var text = document.getElementById("dtPara");
+
+    if (text.style.display === "none") {
+        text.style.display = "block";
+    }
+    else {
+        text.style.display = "none";
+    }
+}
+
+
+function atShow() {
+    var text = document.getElementById("atPara");
+
+    if (text.style.display === "none") {
+        text.style.display = "block";
+    }
+    else {
+        text.style.display = "none";
+    }
+}
