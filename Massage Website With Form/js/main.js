01f58bf3832c7c772496ab1da514daa4ef013500
breknshumway <74122095+breknshumway@users.noreply.github.com>
2020-12-02 12:46:46 -0700

    Added Form

---
+++ Massage Website With Form/js/main.js
@@ -0,0 +1,15 @@
+
+function showHide(paraId) {
+    $('#'+paraId).toggle();
+}
+
+function showHide2(showLyst, hideLyst) {
+    for (let show of showLyst) {
+        $('#'+show).show();
+    }
+
+    for (let hide of hideLyst) {
+        $('#'+hide).hide();
+    }
+
+}
